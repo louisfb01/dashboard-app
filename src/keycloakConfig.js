c5621ec561c2ade9bd2ec3a09af39f9ddc95504c
Louis Mullie <louis.mullie@gmail.com>
2022-11-21 14:17:53 -0500

    Configurable Keycloak client

--- src/keycloakConfig.js
+++ src/keycloakConfig.js
@@ -1,5 +1,5 @@
 export default {
     realm: process.env.VUE_APP_CODA_DASHBOARD_APP_AUTH_REALM,
     url: process.env.VUE_APP_CODA_AUTH_SERVICE_URL,
-    clientId: "dashboard-app"
+    clientId: process.env.VUE_APP_CODA_DASHBOARD_APP_AUTH_CLIENT_ID
 }
